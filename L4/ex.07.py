c6954c4f315682eeada801cbf3a7eae3734103e9
Milton-Pires <mpnn2207@gmail.com>
2025-03-25 22:07:40 -0300

    Add files via upload

---
+++ L4/ex.07.py
@@ -0,0 +1,12 @@
+x=int (input ("Digite um numero: "))
+y=int (input ("Digite um numero: "))
+z=int (input ("Digite um numero: "))
+if z>x and y>x:
+    menor=x
+else:
+    if z>x and z>y:
+        maior=z
+    else:
+        maior=y
+print (menor,)
+        
